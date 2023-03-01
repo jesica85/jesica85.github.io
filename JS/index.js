32a35bd49ae8158045cfda9311c5e82b67a0b3b5
jesica85 <126535206+jesica85@users.noreply.github.com>
2023-03-01 16:00:20 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+document.getElementById("boton").onclick= function(){
+  console.log("capturamos el evento click");
+  document.getElementById("demo").innerHTML="linkedin, faceboock, instagram";
+}
+
+
